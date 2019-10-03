4b066b938eb694c5dba6aaccacd8e2022d49a27a
Thomas Guilleminot <guilleminot.thomas@gmail.com>
2019-10-03 15:50:24 +0200

    feat(develop): add dummy textview in center of main activity

--- app/src/main/java/com/example/uv_today_android/MainActivity.kt
+++ app/src/main/java/com/example/uv_today_android/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.uv_today_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainLabel: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        this.mainLabel = this.findViewById(R.id.main_label)
+        this.mainLabel.text = "UV Today"
     }
 }
