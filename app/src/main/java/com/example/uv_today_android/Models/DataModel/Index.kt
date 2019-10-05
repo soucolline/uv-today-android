db7b7e438d32bd826f520e9bbcaaadc3418943e3
Thomas Guilleminot <guilleminot.thomas@gmail.com>
2019-10-05 14:35:55 +0200

    feat(uv-api): update background color depending on index

--- app/src/main/java/com/example/uv_today_android/Models/DataModel/Index.kt
+++ app/src/main/java/com/example/uv_today_android/Models/DataModel/Index.kt
@@ -1,10 +1,23 @@
 package com.example.uv_today_android.Models.DataModel
 
 import android.content.Context
+import androidx.core.content.ContextCompat
 import com.example.uv_today_android.R
 
 
 typealias Index = Int
+
+fun Index.getAssociatedColorFromContext(context: Context): Int {
+    return when (this) {
+        0 -> ContextCompat.getColor(context, R.color.app_blue)
+        1, 2 -> ContextCompat.getColor(context, R.color.app_green)
+        3, 4, 5 -> ContextCompat.getColor(context, R.color.app_yellow)
+        6, 7 -> ContextCompat.getColor(context, R.color.app_light_red)
+        8, 9, 10 -> ContextCompat.getColor(context, R.color.app_dark_red)
+        11, 12, 13, 14 -> ContextCompat.getColor(context, R.color.app_purple)
+        else -> ContextCompat.getColor(context, R.color.app_black)
+    }
+}
 
 fun Index.getAssociatedDescriptionFromContext(context: Context): String {
     return when (this) {
--- app/src/main/java/com/example/uv_today_android/Views/MainActivity.kt
+++ app/src/main/java/com/example/uv_today_android/Views/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.uv_today_android.Views
 
+import android.animation.ArgbEvaluator
 import android.app.ProgressDialog
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
@@ -7,7 +8,9 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.uv_today_android.Models.DataModel.Index
+import com.example.uv_today_android.Models.DataModel.getAssociatedColorFromContext
 import com.example.uv_today_android.Models.DataModel.getAssociatedDescriptionFromContext
 import com.example.uv_today_android.Presenters.UVPresenter
 import com.example.uv_today_android.Presenters.UVPresenterImpl
@@ -16,9 +19,19 @@ import com.example.uv_today_android.R
 import com.example.uv_today_android.Services.LocationServiceImpl
 import com.example.uv_today_android.Services.UVServiceImpl
 import com.google.android.gms.location.LocationServices
+import android.animation.ObjectAnimator
+import android.graphics.Color
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.toColor
+
 
 class MainActivity : AppCompatActivity(), UVView {
 
+    private lateinit var backgroundView: ConstraintLayout
     private lateinit var cityTextView: TextView
     private lateinit var uvTextView: TextView
     private lateinit var uvDescription: TextView
@@ -30,6 +43,8 @@ class MainActivity : AppCompatActivity(), UVView {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
         this.setupUI()
 
         this.presenter = UVPresenterImpl(LocationServiceImpl(LocationServices.getFusedLocationProviderClient(this), this), UVServiceImpl())
@@ -39,6 +54,7 @@ class MainActivity : AppCompatActivity(), UVView {
     private fun setupUI() {
         this.supportActionBar?.hide()
 
+        this.backgroundView = this.findViewById(R.id.background_view)
         this.cityTextView = this.findViewById(R.id.city_textview)
         this.uvTextView = this.findViewById(R.id.uv_textview)
         this.uvDescription = this.findViewById(R.id.uv_description)
@@ -64,6 +80,7 @@ class MainActivity : AppCompatActivity(), UVView {
     override fun onReceiveSuccess(index: Index) {
         this.uvTextView.text = index.toString()
         this.uvDescription.text = index.getAssociatedDescriptionFromContext(this.applicationContext)
+        this.animateBackgroundColorFromIndex(index)
     }
 
     override fun onReceiveError(error: String) {
@@ -82,5 +99,15 @@ class MainActivity : AppCompatActivity(), UVView {
                 }
             }
         }
+    }
+
+    private fun animateBackgroundColorFromIndex(index: Index) {
+        val colorFrom = ContextCompat.getColor(this.applicationContext, R.color.app_blue)
+        val colorTo = index.getAssociatedColorFromContext(this.applicationContext)
+        val duration = 1000
+
+        ObjectAnimator.ofObject(this.backgroundView, "backgroundColor", ArgbEvaluator(), colorFrom, colorTo)
+            .setDuration(duration.toLong())
+            .start()
     }
 }
